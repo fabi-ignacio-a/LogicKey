4e6e5246d1f45b36934f3f6d2ceaf3f63cdcc16f
fabi_ignacio_ <100646521+fabi-ignacio-a@users.noreply.github.com>
2022-07-21 14:50:58 -0400

    Funcionamiento de las key Proyecto donde se explica el funcionamiento de las key e identificadores.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import "./styles.css";
 
-//La función se llama destructuring
 const Title = ({ children, titleClass }) => {
-  //Las props tienen hijos (children)
   return <h1 className={`${titleClass} title `}> {children} </h1>;
 };
-
+let elements = [
+  { name: "React", id: 1 },
+  { name: "JavaScript", id: 2 },
+  { name: "NestJS", id: 3 },
+  { name: "HTML", id: 4 }
+];
 export default function App() {
   return (
     <div className="App">
       <Title titleClass="green" />
       Me gusta
-      {["React", "JavaScript", "NestJS", "HTML"].map((e) => {
-        return <span style={{ marginRight: 2, marginLeft: 2 }}>{e}</span>;
+      {elements.map((e) => {
+        //Las key o llaves, deben ser unicas. No se pueden repetir los hijos
+        //Se aplica, generalmente, en las listas
+        //Usar index solo es cuando las listas no varian o cambian.
+        return (
+          <span key={e.id} style={{ marginRight: 2, marginLeft: 2 }}>
+            {e.name}
+          </span>
+        );
       })}
       <Title />
-      {/* 
-        Los children pueden recibir lo que uno quiera como valor.
-        Incluso pueden tener HTML.
-        */}
     </div>
   );
 }
